445f27fa470f13bd10de842eede31693ba00cc7b
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-01-08 11:26:28 +0100

    [EUREKA REGISTRY S15] Load balancing with WebClient

--- java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/webclient/WebClientConfig.java
+++ java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/webclient/WebClientConfig.java
@@ -1,5 +1,6 @@
 package fr.adriencournand.formation.consumer.webclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,8 +9,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
+    public WebClient.Builder LoadBalancedWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
     public WebClient WebClient(WebClient.Builder builder) {
-        return builder.baseUrl("http://localhost:8081").build();
+        return builder.baseUrl("http://provider").build();
     }
 
 }
--- java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/webclient/WebClientController.java
+++ java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/webclient/WebClientController.java
@@ -3,7 +3,6 @@ package fr.adriencournand.formation.consumer.webclient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
