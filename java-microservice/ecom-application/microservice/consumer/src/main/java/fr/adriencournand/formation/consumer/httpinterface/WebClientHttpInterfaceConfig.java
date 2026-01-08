98d4e7d81f1be7f3bb94f549caf2e2d4388d0714
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-01-08 12:11:27 +0100

    [EUREKA REGISTRY S15] Load balancing with http interface and WebClient

--- java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/httpinterface/WebClientHttpInterfaceConfig.java
+++ java-microservice/ecom-application/microservice/consumer/src/main/java/fr/adriencournand/formation/consumer/httpinterface/WebClientHttpInterfaceConfig.java
@@ -1,5 +1,7 @@
 package fr.adriencournand.formation.consumer.httpinterface;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,9 +12,16 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 @Configuration
 public class WebClientHttpInterfaceConfig {
 
+    @Bean("httpInterfaceWebClientBuilder")
+    @LoadBalanced
+    WebClient.Builder HttpInterfaceWebClientBuilder() {
+        return WebClient.builder();
+    }
+
     @Bean("webClientInterface")
-    public IHttpInterfaceProvider WebClientHttpInterface() {
-        WebClient client = WebClient.builder().baseUrl("http://localhost:8081").build();
+    public IHttpInterfaceProvider WebClientHttpInterface(
+            @Qualifier("httpInterfaceWebClientBuilder") WebClient.Builder builder) {
+        WebClient client = builder.baseUrl("http://provider").build();
         WebClientAdapter adapter = WebClientAdapter.create(client);
         HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory.builderFor(adapter).build();
 
