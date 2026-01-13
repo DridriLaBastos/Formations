e5b434f528a8598df7711d6a7245e910e81f9722
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-01-13 10:54:24 +0100

    [GATEWAY S18] Filtering any incoming request

---
+++ java-microservice/ecom-application/microservice/gateway/src/main/java/fr/adriencournand/formation/gateway/LoggingFilter.java
@@ -0,0 +1,21 @@
+package fr.adriencournand.formation.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Incoming request {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
