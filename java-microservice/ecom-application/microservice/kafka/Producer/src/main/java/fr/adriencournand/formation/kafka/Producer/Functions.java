71e9c6fc7ce25e64af5367990b92c693c941c783
Adrien COURNAND <19534959+DridriLaBastos@users.noreply.github.com>
2026-01-19 16:44:12 +0100

    [KAFKA S21] Spring Cloud Function

---
+++ java-microservice/ecom-application/microservice/kafka/Producer/src/main/java/fr/adriencournand/formation/kafka/Producer/Functions.java
@@ -0,0 +1,15 @@
+package fr.adriencournand.formation.kafka.Producer;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Functions {
+
+    @Bean
+    public Function<String,String> uppercase () {
+        return value -> value.toUpperCase();
+    }
+}
